36861946c3de1f4981034154a081fe2d2c4fed1f
jeff1999git <jeffinmjtcr@gmail.com>
2024-04-03 10:59:37 +0530

    eventApp started

---
+++ android/app/src/main/kotlin/com/example/event_app_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.event_app_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:event_app_mobile/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
