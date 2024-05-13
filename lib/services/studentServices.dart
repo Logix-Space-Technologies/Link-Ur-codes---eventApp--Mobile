287cbfc3478e221a911565d2d8272182a3b4534a
Annateresa01 <shajianna013@gmail.com>
2024-05-13 23:14:42 +0530

    ok

--- lib/services/studentServices.dart
+++ lib/services/studentServices.dart
@@ -1,3 +1,4 @@
+import 'package:event_app_mobile/models/studentModel.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -45,4 +46,68 @@ class StudentApiService {
       };
     }
   }
-}
+
+  Future<Map<String, dynamic>> forgotpassword(String email) async {
+    final url = Uri.parse('http://localhost:8085/api/student/forgotpassword');
+    try {
+      final response = await http.post(
+        url,
+        body: jsonEncode({'student_email': email}),
+        headers: {'Content-Type': 'application/json'},
+      );
+      if (response.statusCode == 200) {
+        // Request was successful, parse and return the response body
+        return jsonDecode(response.body);
+      } else {
+        // Request failed with a non-200 status code
+        print('Request failed with status: ${response.statusCode}');
+        return {'success': false, 'message': 'Failed to reset password'};
+      }
+    } catch (error) {
+      // Request failed due to an error (e.g., network error)
+      print('Request failed with error: $error');
+      return {'success': false, 'message': 'Failed to reset password'};
+    }
+  }
+
+
+  Future<List<Student>> getstud(String student_id,token) async {
+    print(token);
+    var client = http.Client();
+    var apiUri = Uri.parse("http://localhost:8085/api/student/viewstud1");
+
+    try {
+      var response = await client.post(
+        apiUri,
+        headers: <String, String>{
+          "Content-Type": "application/json; charset=UTF-8",
+          "token":token
+        },
+        body: jsonEncode(<String, String>{
+          "student_id": student_id
+
+        }),
+      );
+
+      // Check if the response status code is 200 (OK)
+      if (response.statusCode == 200) {
+        // Parse the response body into a list of Student objects
+        List<dynamic> responseBody = jsonDecode(response.body);
+        List<Student> students = responseBody.map((json) => Student.fromJson(json)).toList();
+
+        return students;
+      } else {
+        // If the server returns an error response, throw an exception
+        throw Exception('Failed to load students: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle any errors that occur during the HTTP request
+      print('Error: $e');
+      throw Exception('Failed to load students');
+    } finally {
+      // Close the client to free up resources
+      client.close();
+    }
+  }
+
+    }
