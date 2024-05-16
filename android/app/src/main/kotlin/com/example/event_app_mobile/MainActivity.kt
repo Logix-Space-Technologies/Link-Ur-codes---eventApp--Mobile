337eef76042162d85f5cdc5161e7cdc38bb50e8c
anexjnr <119106239+anexjnr@users.noreply.github.com>
2024-05-17 00:05:51 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/event_app_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/event_app_mobile/MainActivity.kt
@@ -1,5 +1,40 @@
 package com.example.event_app_mobile
 
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.Manifest
+import android.content.pm.PackageManager
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val PERMISSION_REQUEST_CODE = 1
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Permission granted
+            } else {
+                // Permission denied
+            }
+        }
+    }
+}
