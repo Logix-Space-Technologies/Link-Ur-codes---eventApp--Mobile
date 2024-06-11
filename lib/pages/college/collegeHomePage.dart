6b936ab46284328813cbba089a24ce589be607bb
anexjnr <119106239+anexjnr@users.noreply.github.com>
2024-06-11 18:04:03 +0530

    Update collegeHomePage.dart

--- lib/pages/college/collegeHomePage.dart
+++ lib/pages/college/collegeHomePage.dart
@@ -1,6 +1,7 @@
 import 'package:event_app_mobile/pages/college/profilePage.dart';
 import 'package:event_app_mobile/pages/college/studentDetails.dart';
 import 'package:flutter/material.dart';
+import 'certificatePage.dart';
 import 'eventPage.dart';
 import 'eventhistory.dart';
 
@@ -14,7 +15,7 @@ class _CollegeMenuState extends State<CollegeMenu> {
   int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
     ProfilePage(),
-    //StudentDetailsPage(),
+    CertificatePage(),
     EventPage(),
     EventHistoryPage()
   ];
@@ -28,10 +29,6 @@ class _CollegeMenuState extends State<CollegeMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('College',style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.black,
-      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
@@ -41,10 +38,6 @@ class _CollegeMenuState extends State<CollegeMenu> {
             icon: Icon(Icons.person),
             label: 'Profile',
           ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.people),
-          //   label: 'Student Details',
-          // ),
           BottomNavigationBarItem(
            icon: Icon(Icons.event),
              label: 'Events',
@@ -52,6 +45,10 @@ class _CollegeMenuState extends State<CollegeMenu> {
           BottomNavigationBarItem(
             icon: Icon(Icons.history),
             label: 'Event History',
+          ),
+          BottomNavigationBarItem(
+          icon: Icon(Icons.insert_page_break_outlined),
+          label: 'Certificates',
           ),
         ],
         currentIndex: _selectedIndex,
@@ -62,17 +59,3 @@ class _CollegeMenuState extends State<CollegeMenu> {
     );
   }
 }
-
-
-// class EventsPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Text(
-//         'Event Page',
-//         style: TextStyle(fontSize: 20),
-//       ),
-//     );
-//   }
-// }
-
